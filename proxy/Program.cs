f542ce314d21f8b655317ce8c18a5255f13b279f
iailyas <62544436+iailyas@users.noreply.github.com>
2021-08-30 20:35:25 +0300

    add proj

---
+++ proxy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using System.Threading;
+
+namespace kr2ks
+{
+    public class WebRequestGetExample
+    {
+        public static void Main()
+        {
+
+
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.2"), 13003);
+            server.Start();
+            while (true)
+            {
+                //server.AcceptSocket();
+                TcpClient client = server.AcceptTcpClient();
+
+
+
+                Console.WriteLine("Connected!");
+
+
+                // Get a stream object for reading and writing
+
+                NetworkStream stream = client.GetStream();
+                Byte[] bytesClient = new Byte[256000];
+                stream.Read(bytesClient, 0, bytesClient.Length);
+                string ClientMessage = null;
+                ClientMessage = System.Text.Encoding.ASCII.GetString(bytesClient, 0, bytesClient.Length);
+                Console.WriteLine(ClientMessage);
+                stream.Write(bytesClient, 0, bytesClient.Length);
+                client.Close();
+
+            }
+
+        }
+    }
+}
